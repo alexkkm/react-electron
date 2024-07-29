a49bb774d126d3dd382adb23907db3c586773317
alexkkm <alexkong0222@gmail.com>
2024-07-29 15:47:22 +0800

    place the darkthemeMessage as root element

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,36 +1,57 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import Versions from "./components/Versions";
+import DarkThemePage from "./components/darktheme";
+
+import electronLogo from "./assets/electron.svg";
+import "./App.css";
 
 function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
-
-  return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-          <a onClick={() => { console.log("hi") }}>Hi</a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
-  )
+	return <DarkThemePage />;
 }
 
-export default App
+const Old = () => {
+	const ipcHandle = () => window.electron.ipcRenderer.send("ping");
 
+	return (
+		<>
+			<img
+				alt="logo"
+				className="logo"
+				src={electronLogo}
+			/>
+			<div className="creator">Powered by electron-vite</div>
+			<div className="text">
+				Build an Electron app with <span className="react">React</span>
+			</div>
+			<p className="tip">
+				Please try pressing <code>F12</code> to open the devTool
+			</p>
+			<div className="actions">
+				<div className="action">
+					<a
+						href="https://electron-vite.org/"
+						target="_blank"
+						rel="noreferrer">
+						Documentation
+					</a>
+				</div>
+				<div className="action">
+					<a
+						target="_blank"
+						rel="noreferrer"
+						onClick={ipcHandle}>
+						Send IPC
+					</a>
+					<a
+						onClick={() => {
+							console.log("hi");
+						}}>
+						Hi
+					</a>
+				</div>
+			</div>
+			<Versions></Versions>
+		</>
+	);
+};
+
+export default App;
---
+++ src/renderer/src/components/darktheme.jsx
@@ -0,0 +1,20 @@
+import "./darktheme.css";
+
+const DarkThemePage = () => {
+	return (
+		<div class="darkThemePage">
+			<div class="messageBoard">
+				<div class="pad">
+					<div class="pad__body">
+						<h4 class="text-heading3 undefined">Title of the message board</h4>
+						<p class="text-paragraph1 undefined">Para 1</p>
+						<p class="text-paragraph1 undefined">
+							Paragraph 2 which is trying to have more than one line.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default DarkThemePage;
