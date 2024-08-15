3662475db7301c82db63ee9292d1d75397fbb559
alexkkm <alexkong0222@gmail.com>
2024-08-15 10:42:06 +0800

    edit TodoList.jsx for crud

--- src/renderer/src/components/TodoList.jsx
+++ src/renderer/src/components/TodoList.jsx
@@ -9,17 +9,27 @@ const TodoListWidget = (parameters) => {
 
 
     const writeLocal = () => {
-        // store data in local storage, setItem("key","value"), while "value" can only be string
+        // create data in local storage, setItem("key","value"), while "value" can only be string
         localStorage.setItem("field1", JSON.stringify({ abc: 2, def: "24" }));
     }
 
     const readLocal = () => {
+        // read data with specific field "field1" in local storage
         const buf = localStorage.getItem("field1");
         console.log(buf)
     }
 
+    const deleteLocal = () => {
+        // delete specific field of data in local storage
+        localStorage.removeItem("field1");
+
+    }
+    const clearLocal = () => {
+        // clear all data in local storage
+        localStorage.clear();
+    }
+
     useEffect(() => {
-        writeLocal();
         readLocal();
     }, []);
 
